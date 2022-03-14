4f9522caf0a96e8466bb2669e9bb83a7f9d536a3
Ezequiel Tillerias Ley Matias Bastian <matias.tillerias@conexlan.cl>
2022-03-14 14:28:06 -0300

    saving info into db

--- DomainAnalize.py
+++ DomainAnalize.py
@@ -3,22 +3,46 @@ from rich.console import Console
 from rich.progress import track
 import argparse
 import sqlite3
-
+from sqlite3 import Error
 console = Console()
 
 
 parser = argparse.ArgumentParser(description="Intelligence gathering based on a list of subdomains")
 
 parser.add_argument('-l','--list', help='List with subdomains')
+parser.add_argument('-n','--name', help='Name of the project')
+
 args = parser.parse_args()
+
+def database():
+    con = sqlite3.connect(args.name)
+    db = con.cursor()
+    db.execute('''CREATE TABLE IF NOT EXISTS domain(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            domain,
+            ip
+            )''')
+    return con
 def Getip():
+    con = database()
+    db = con.cursor()
     dominios = open(args.list).readlines()
     numerOfDomains = len(dominios)
-    for i in track(range(numerOfDomains)):
+    domainIpList = []
+    for i in track(range(numerOfDomains), description="Getting the ip address"):
         current_domain = dominios[i].rstrip("\n")
         try:
             ip = socket.gethostbyname(current_domain)
-            console.log("[yellow] " +current_domain+"[/yellow][green] "+ip+"[/green]")
+            domainIpList.append(current_domain)
+            domainIpList.append(ip)
+            domainIpList = tuple(domainIpList)
+            try:
+                db.execute("INSERT INTO domain (domain,ip) VALUES (?,?)", domainIpList)
+                con.commit()
+                domainIpList= []
+            except Error as e:
+                console.log(e)
+            console.log("\n[yellow] " +current_domain+"[/yellow][green] "+ip+"[/green]")
         except:
-            console.log("[red] " + current_domain + " does not have ip addres on the A record")
+            console.log("\n[red] " + current_domain + " does not have ip address on the A record")
 Getip()
